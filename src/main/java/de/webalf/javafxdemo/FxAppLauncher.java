3e0e5390f14b01684e4ec824d40878328a8d15ff
Alf <62615304+alf-melmac@users.noreply.github.com>
2022-10-12 23:05:02 +0200

    JavaFX example
    Signed-off-by: Alf <62615304+alf-melmac@users.noreply.github.com>

---
+++ src/main/java/de/webalf/javafxdemo/FxAppLauncher.java
@@ -0,0 +1,12 @@
+package de.webalf.javafxdemo;
+
+/**
+ * Wrapper main class as shown on <a href="https://stackoverflow.com/a/57691362/16201869">StackOverflow</a>
+ * <p>
+ * To run this application inside an IDE like IntelliJ it may be necessary to enable the module-info file. See <a href="https://openjfx.io/openjfx-docs/#IDE-Intellij">JavaFX and IntelliJ -> Non-modular with Maven</a>
+ */
+public class FxAppLauncher {
+	public static void main(String[] args) {
+		HelloFX.main(args);
+	}
+}
